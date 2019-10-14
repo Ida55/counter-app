bd86dac6213e1d6765e22d0c6c5a3916483f9b27
ida.pousse <idapousse@yahoo.fr>
2019-10-14 19:06:46 +0200

    Add decrement feature

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0,
+        valuee: this.props.valuee
     };
 
     // constructor(){
@@ -10,34 +10,41 @@ class Counter extends Component {
     //     this.handleIncrement = this.handleIncrement.bind(this);
     // }
 
-    handleIncrement = product => {
-        this.setState({ count: this.state.count + 1 })
+    handleIncrement = () => {
+        this.setState({ valuee: this.state.valuee + 1 })
+    }
+    
+    handleDecrement = () => {
+        this.setState({ valuee: this.state.valuee - 1 })
     }
 
-    
 
     render() { 
-    
+        
         return (
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={(product) => this.handleIncrement(product)} 
+                <button onClick={() => this.handleIncrement()} 
                     className="btn btn-secondary btn-sm">
                     Increment
-                    </button>
+                </button>
+                <button onClick={() => this.handleDecrement()} 
+                    className="btn btn-secondary btn-sm m-2">
+                    Decrement
+                </button>
             </div>);
     }
 
     getBadgeClasses() {
         let classes = "badge m-2 badge"; // in all cases we want to have a badge with a margin of 2 
-        classes += (this.state.count === 0) ? "-warning" : "-primary"; // to which we add the color blue or yellow depending on the state of the count property 
+        classes += (this.state.valuee === 0) ? "-warning" : "-primary"; // to which we add the color blue or yellow depending on the state of the count property 
         // let classes = (this.state.count === 0) ? "badge badge-warning m-2" : "badge badge-primary m-2";
         return classes;
     }
 
     formatCount() {
-        const {count} = this.state // Object destructuring : picking the 'count' property of the state object & storing it into a constant and setting it to the current state object (-> this.state)
-        return count === 0 ? 'Zero' : count;
+        const {valuee} = this.state // Object destructuring : picking the 'count' property of the state object & storing it into a constant and setting it to the current state object (-> this.state)
+        return valuee === 0 ? 'Zero' : valuee;
     }
 
 }
