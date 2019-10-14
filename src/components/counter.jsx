0510b1a19c9c631bd03f3b9107eba9ad3b4c617d
ida.pousse <idapousse@yahoo.fr>
2019-10-14 10:59:16 +0200

    Add function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,10 +8,16 @@ class Counter extends Component {
         
         return (
         <div>
-            <span>{this.state.count}</span>
+            <span>{this.formatCount()}</span>
             <button>Increment</button>
         </div>);
     }
+
+    formatCount() {
+        const {count} = this.state // Object destructuring : picking the 'count' property of the state object & storing it into a constant and setting it to the current state object (-> this.state)
+        return count === 0 ? 'Zero' : count;
+    }
+
 }
  
 export default Counter;
