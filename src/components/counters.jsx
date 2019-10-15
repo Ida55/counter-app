2ef60d4895a50dd7226464eb1522571b2d16c672
ida.pousse <idapousse@yahoo.fr>
2019-10-15 18:05:36 +0200

    Update

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,7 +4,7 @@ import Counter from './counter';
 class Counters extends Component {
     state = { 
         counters: [
-            { id: 1, value: 4},
+            { id: 1, value: 0},
             { id: 2, value: 0},
             { id: 3, value: 0},
             { id: 4, value: 0},
@@ -44,7 +44,7 @@ class Counters extends Component {
     render() { 
         return ( 
         <div >
-            <button  onClick={this.handleReset} className="btn btn-primary btn-sm m-2">Reset</button>
+        <button onClick={this.handleReset} className="btn btn-primary btn-sm m-2">Reset</button>
             {this.state.counters.map(counter => ( 
                 <Counter 
                 key={counter.id} 
