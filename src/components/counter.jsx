6f25ce1437025123e246cb76d4c2c4b527f2b253
ida.pousse <idapousse@yahoo.fr>
2019-10-15 16:11:17 +0200

    Add raise & handle events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,49 +2,47 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        valuee: this.props.valuee
+        value: this.props.counter.value
     };
 
-    // constructor(){
-    //     super()
-    //     this.handleIncrement = this.handleIncrement.bind(this);
-    // }
-
     handleIncrement = () => {
-        this.setState({ valuee: this.state.valuee + 1 })
+        this.setState({ value: this.state.value + 1 })
     }
     
     handleDecrement = () => {
-        this.setState({ valuee: this.state.valuee - 1 })
+        this.setState({ value: this.state.value - 1 })
     }
-
 
     render() { 
         
         return (
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={() => this.handleIncrement()} 
+                <button onClick={this.handleIncrement} 
                     className="btn btn-secondary btn-sm">
                     Increment
                 </button>
-                <button onClick={() => this.handleDecrement()} 
+                <button onClick={this.handleDecrement} 
                     className="btn btn-secondary btn-sm m-2">
                     Decrement
+                </button>
+                <button onClick={() => this.props.onDelete(this.props.counter.id)} 
+                    className="btn btn-danger btn-sm m-2">
+                    Delete
                 </button>
             </div>);
     }
 
     getBadgeClasses() {
         let classes = "badge m-2 badge"; // in all cases we want to have a badge with a margin of 2 
-        classes += (this.state.valuee === 0) ? "-warning" : "-primary"; // to which we add the color blue or yellow depending on the state of the count property 
+        classes += (this.state.value === 0) ? "-warning" : "-primary"; // to which we add the color blue or yellow depending on the state of the count property 
         // let classes = (this.state.count === 0) ? "badge badge-warning m-2" : "badge badge-primary m-2";
         return classes;
     }
 
     formatCount() {
-        const {valuee} = this.state // Object destructuring : picking the 'count' property of the state object & storing it into a constant and setting it to the current state object (-> this.state)
-        return valuee === 0 ? 'Zero' : valuee;
+        const {value} = this.state // Object destructuring : picking the 'count' property of the state object & storing it into a constant and setting it to the current state object (-> this.state)
+        return value === 0 ? 'Zero' : value;
     }
 
 }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,11 +11,30 @@ class Counters extends Component {
         ]
      };
 
+     // map method to get each counter and reset its value -> we get the existing counters w. the map method to get each counter & reset its value to 0
+     handleReset = () => {
+       const counters =  this.state.counters.map(c => {
+            c.value = 0;
+            return c;
+        });
+        this.setState({counters});
+     };
+
+    handleDelete = (counterId) => {
+        const counters = this.state.counters.filter(c => c.id !== counterId);
+        this.setState({ counters: counters });
+    };
+
     render() { 
         return ( 
         <div >
-            {this.state.counters.map(counter => 
-                <Counter key={counter.id} valuee={counter.value}/>)}
+            <button  onClick={this.handleReset} className="btn btn-primary btn-sm m-2">Reset</button>
+            {this.state.counters.map(counter => ( 
+                <Counter 
+                key={counter.id} onDelete={this.handleDelete} 
+                counter={counter}
+                />
+                ))}
         </div> 
         );
     }
