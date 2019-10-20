3d5cf913c88316fc2c7c5716f2e9ef27776ea55d
ida.pousse <idapousse@yahoo.fr>
2019-10-20 13:37:15 +0200

    Update comment

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    // Stateless component (controlled) that relies on props to receive needed data
+    // Stateless (controlled) component that relies on props to receive its needed data -> we've passed the state of the counters component to the counter component as props
     render() { 
         return (
             <div>
@@ -22,14 +22,14 @@ class Counter extends Component {
     }
 
     getBadgeClasses() {
-        let classes = "badge m-2 badge"; // in all cases we want to have a badge with a margin of 2 
-        classes += (this.props.counter.value === 0) ? "-warning" : "-primary"; // to which we add the color blue or yellow depending on the state of the count property 
+        let classes = "badge m-2 badge";
+        classes += (this.props.counter.value === 0) ? "-warning" : "-primary"; // to which we add the color blue or yellow depending on the state of the value property 
         // let classes = (this.state.count === 0) ? "badge badge-warning m-2" : "badge badge-primary m-2";
         return classes;
     }
 
     formatCount() {
-        const {value} = this.props.counter // Object destructuring : picking the 'count' property of the state object & storing it into a constant and setting it to the current state object (-> this.state)
+        const {value} = this.props.counter // Object destructuring : picking the 'value' property of the state object & storing it into a constant then setting it to the current state object (-> this.state)
         return value === 0 ? 'Zero' : value;
     }
 
