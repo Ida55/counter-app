83ab5a0b3343a1b8cd04ae474efe3189f18d35e2
ida.pousse <idapousse@yahoo.fr>
2019-10-20 15:27:47 +0200

    Add prop

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
-    render() { 
-        return ( 
-            <nav className="navbar navbar-light bg-light">
-                <a className="navbar-brand" href="#">Cart</a>
-            </nav>
-         );
-    }
+// Stateless Functional Component
+
+const NavBar = (props) => {
+
+    return ( 
+        <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand" href="#">NavBar 
+            <span className="badge badge-pill badge-secondary">
+            {props.totalCounters}
+            </span> 
+            </a>
+        </nav>
+     );
+
 }
- 
+
 export default NavBar;
